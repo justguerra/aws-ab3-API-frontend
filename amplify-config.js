04f77e2f791d5429cff2a3761e8181cc7fd98272
justguerra <justguerra@users.noreply.github.com>
2022-06-25 21:10:15 -0300

    Initial Commit

---
+++ amplify-config.js
@@ -0,0 +1,28 @@
+// This file is used for manual configuration of the Amplify library.
+// When Amplify is used in conjunction with the Amplify CLI toolchain or AWS Mobile Hub to manage backend resources,
+// an aws-exports.js file is auto-generated and can be used instead of the below to automatically configure the Amplify library.
+// In this workshop, we are using the Amplify client libraries without the CLI toolchain so you should edit this file manually.
+
+const awsConfig = {
+    Auth: {
+        identityPoolId: 'us-east-1:46a2761b-18ab-4f94-9377-480dc05877c7', // example: 'us-east-2:c85f3c18-05fd-4bb5-8fd1-e77e7627a99e'
+        region: 'us-east-1', // example: 'us-east-2'
+        userPoolId: 'us-east-1_GG5D1CX5N', // example: 'us-east-2_teEUQbkUh'
+        userPoolWebClientId: '3ubei9t3cc8lij4p3kombb9juf' // example: '3k09ptd8kn8qk2hpk07qopr86'
+    },
+    API: {
+        endpoints: [
+            {
+                name: 'WildRydesAPI',
+                endpoint: 'https://wcdscorz61.execute-api.us-east-1.amazonaws.com/prod', // example: 'https://u8swuvl00f.execute-api.us-east-2.amazonaws.com/prod'
+                region: 'us-east-1' // example: 'us-east-2'
+            }
+        ]
+    },
+    Storage: {
+        bucket: 'serverless-idm-backend-profilepicturesbucket-1q45c5ma27svr', //example: 'wildrydesbackend-profilepicturesbucket-1wgssc97ekdph'
+        region: 'us-east-1' // example: 'us-east-2'
+    }
+}
+
+export default awsConfig;
---
+++ index.js
@@ -0,0 +1,43 @@
+import { Amplify, Auth } from "aws-amplify";
+import awsconfig from "./amplify-config";
+Amplify.configure(awsconfig);
+
+const usuario = "rodrigoguerra";
+const senha = "Qwert!321";
+const mail = "test@foxway.com.br";
+
+async function signUp() {
+  try {
+    const { user } = await Auth.signUp({
+      username: usuario,
+      password: senha,
+      attributes: {
+        email: mail, // optional
+        // phone_number, // optional - E.164 number convention
+        // // other custom attributes
+        // 'custom:favorite_flavor': 'Cookie Dough'  // custom attribute, not standard
+      },
+    });
+    console.log(user);
+  } catch (error) {
+    console.log("error signing up:", error);
+  }
+}
+cognitoUser = signIn();
+
+cognitoUser.then();
+
+async function signIn() {
+  try {
+    const user = await Auth.signIn("rodrigoguerra", "Qwert!321");
+    console.log(user.signInUserSession.accessToken);
+    console.log(user.signInUserSession.idToken);
+    console.log(user.signInUserSession.refreshToken);
+    console.log("logou com o usuário:" + user.username);
+    console.log("logou com o usuário:" + user.attributes.email);
+    console.log("logou com o usuário:" + user.pool.userPoolId);
+    console.log(user);
+  } catch (error) {
+    console.log("error signing in", error);
+  }
+}
