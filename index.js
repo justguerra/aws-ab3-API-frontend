b48dc1f10be43b510cd290b304b730bdf1d3582d
justguerra <justguerra@users.noreply.github.com>
2022-07-05 02:48:26 -0300

    Backends implemented

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import awsconfig from "./amplify-config";
 const usuario = "rodrigoguerra";
 const senha = "Qwert!321";
 const mail = "test@foxway.com.br";
-
+let user = "";
 //Enviar dados de Login para o Cognito Auth
 //Salvar Tokens de Autenticacacao
 //Usar Tokens para acessar o API Gateway
@@ -14,12 +14,39 @@ const mail = "test@foxway.com.br";
   const form = document.querySelector(".form");
   const email = document.querySelector(".email");
   const password = document.querySelector(".password");
+  const addDogBtn = document.querySelector(".add-dog-btn");
+  const addCatBtn = document.querySelector(".add-cat-btn");
+  let loginStatusForm = document.querySelector(".login-status-form");
   let formStatus = 0;
 
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
+    document.querySelector(".cat").classList.add("invisible");
+    document.querySelector(".dog").classList.add("invisible");
+    document.querySelector(".user-mod").classList.add("invisible");
+
+    loginStatusForm.innerHTML = "";
     try {
-      tenantVerifier(email.value);
+      updateMsg("Descobrindo de qual empresa você pertence!");
+      tenantVerifier(email.value, password.value);
+      //const res = await signIn(cognitoUser, email.value, password.value);
+    } catch (e) {
+      console.log("Login fail. Error: ", e);
+    }
+  });
+
+  addDogBtn.addEventListener("click", async (event) => {
+    try {
+      apiAccess(false, "dogs");
+      //const res = await signIn(cognitoUser, email.value, password.value);
+    } catch (e) {
+      console.log("Login fail. Error: ", e);
+    }
+  });
+
+  addCatBtn.addEventListener("click", async (event) => {
+    try {
+      apiAccess(false, "cats");
       //const res = await signIn(cognitoUser, email.value, password.value);
     } catch (e) {
       console.log("Login fail. Error: ", e);
@@ -27,7 +54,44 @@ const mail = "test@foxway.com.br";
   });
 })();
 
-async function tenantVerifier(email) {
+async function signIn(email, senha) {
+  try {
+    console.log(email.replace(/[@.]/g, "|"));
+    user = await Auth.signIn(email.replace(/[@.]/g, "|"), senha);
+    // const user = await Auth.signIn("rodrigoguerra", "Qwert!321");
+    console.log(user.signInUserSession.accessToken);
+    console.log(user.signInUserSession.idToken);
+    console.log(user.signInUserSession.refreshToken);
+    console.log("logou com o usuário:" + user.username);
+    console.log("logou com o usuário:" + user.attributes.email);
+    console.log("logou com o usuário:" + user.pool.userPoolId);
+    updateMsg("Logado com sucesso!", "success");
+    updateMsg("Seu usuário é " + user.username);
+
+    console.log(user);
+    let user2 = Object.getPrototypeOf(user);
+
+    let clientID = user.pool.userPoolId;
+    let dominio = email.split("@")[1].replace(/[@.]/g, "|");
+    let JWTlocal =
+      localStorage[
+        "CognitoIdentityServiceProvider." +
+          clientID +
+          "." +
+          dominio +
+          ".accessToken"
+      ];
+    let jwt = user.signInUserSession.accessToken.jwtToken;
+    apiAccess(jwt, "dogs");
+    apiAccess(jwt, "users");
+
+    apiAccess(jwt, "cats");
+  } catch (error) {
+    console.log("error signing in", error);
+    updateMsg("Sua senha está errada", "text-danger");
+  }
+}
+async function tenantVerifier(email, senha) {
   console.log("Inicia Verificacao do Tenant");
   lambdaCognitoTenantUrl =
     "https://0ywa8nsqe7.execute-api.us-east-1.amazonaws.com/cognitoTenant?email=";
@@ -45,6 +109,9 @@ async function tenantVerifier(email) {
     newAmplifyConfig.Auth.userPoolId = data.Item.userPoolId;
     newAmplifyConfig.Auth.userPoolWebClientId = data.Item.userPoolWebClientId;
     console.log("Retorna Amplify Config Atualizado");
+    updateMsg("Sua empresa é a " + data.Item.domain);
+    updateMsg("Tenant 00" + data.Item.tenant_id);
+
     console.log(newAmplifyConfig);
     Amplify.configure(newAmplifyConfig);
     return newAmplifyConfig;
@@ -52,7 +119,7 @@ async function tenantVerifier(email) {
 
   let cognitoTenant = await fetchAsyncGetCognito(finalURL);
 
-  signIn(email);
+  signIn(email, senha);
 }
 
 async function signUp() {
@@ -76,51 +143,147 @@ async function signUp() {
 
 // cognitoUser.then();
 
-async function signIn(email) {
-  try {
-    console.log(email.replace(/[@.]/g, "|"));
-    const user = await Auth.signIn(email.replace(/[@.]/g, "|"), "Qwert!23");
-    // const user = await Auth.signIn("rodrigoguerra", "Qwert!321");
-    console.log(user.signInUserSession.accessToken);
-    console.log(user.signInUserSession.idToken);
-    console.log(user.signInUserSession.refreshToken);
-    console.log("logou com o usuário:" + user.username);
-    console.log("logou com o usuário:" + user.attributes.email);
-    console.log("logou com o usuário:" + user.pool.userPoolId);
-    console.log(user);
-    user.refreshToken;
-    let user2 = Object.getPrototypeOf(user);
-
-    let clientID = user.pool.userPoolId;
-    let dominio = email.split("@")[1].replace(/[@.]/g, "|");
-    let JWTlocal =
-      localStorage[
-        "CognitoIdentityServiceProvider." +
-          clientID +
-          "." +
-          dominio +
-          ".accessToken"
-      ];
-    apiAccess(user.signInUserSession.accessToken.jwtToken);
-  } catch (error) {
-    console.log("error signing in", error);
-  }
+function updateMsg(msg, style) {
+  const loginStatusForm = document.querySelector(".login-status-form");
+  loginStatusForm.insertAdjacentHTML(
+    "beforeend",
+    '<li class="' +
+      "list-group-item list-group-item-" +
+      (style || "light") +
+      '" >' +
+      msg +
+      "</li>"
+  );
 }
 
-async function apiAccess(JWT) {
-  const urlAPI =
-    "https://y5xre89ndl.execute-api.us-east-1.amazonaws.com/prod/users";
+async function apiAccess(JWT, path) {
+  let primeiroAcesso = true;
+  if (JWT == false) {
+    JWT = user.signInUserSession.accessToken.jwtToken;
+    primeiroAcesso = false;
+    console.log(JWT);
+  }
+  const urlAPI = "https://y5xre89ndl.execute-api.us-east-1.amazonaws.com/prod/";
   try {
-    let response = await fetch(urlAPI, {
+    let response = await fetch(urlAPI + path, {
       headers: {
         "Content-Type": "application/json",
         Authorization: JWT,
       },
     });
-
     let data = await response.json();
-    console.log(data);
+    if (response.status == 403) {
+      updateMsg("Pacote " + path + " não adquirido!", "warning ");
+      updateMod(path, data, false);
+    } else if (response.status == 200) {
+      if (primeiroAcesso) {
+        updateMsg("Pacote " + path + " adquirido!", "info ");
+      }
+      console.log(data);
+      updateMod(path, data, true);
+    }
   } catch (error) {
-    console.log("error acessando backend in", error);
+    console.log("error acessando backend in " + (path || "users"), error);
+    updateMsg("Erro acessando o pacote " + path + " !", "danger ");
+  }
+}
+
+function updateMod(path, data, hasAccess) {
+  switch (path) {
+    case "cats":
+      updateCat(data, hasAccess);
+      break;
+    case "users":
+      updateUsers(data, hasAccess);
+      break;
+
+    case "dogs":
+      updateDog(data, hasAccess);
+      break;
+    default:
+      break;
+  }
+}
+
+function updateCat(data, hasAccess) {
+  const catElement = document.querySelector(".cat");
+  const catH6 = document.querySelector(".cat h6");
+  const catDiv = document.querySelector(".cat div");
+  const catBtn = document.querySelector(".cat button");
+  catElement.classList.remove("invisible");
+
+  if (hasAccess) {
+    catBtn.classList.remove("invisible");
+    catElement.classList.remove("sem-acesso");
+
+    catH6.innerHTML = "Gatos, veja fotos de gatos.";
+
+    const catElemt = document.querySelector(".catImage");
+    let catUrl = "https://cataas.com" + data.url;
+    catElemt.insertAdjacentHTML(
+      "beforeend",
+      '<img src="' + catUrl + '" alt="" >'
+    );
+  } else {
+    catElement.classList.add("sem-acesso");
+
+    catBtn.classList.add("invisible");
+    catH6.innerHTML = "Sem Acesso";
+    catDiv.innerHTML = "";
+  }
+}
+
+function updateUsers(data, hasAccess) {
+  const userElement = document.querySelector(".usr-table");
+  const userDiv = document.querySelector(".user-mod");
+  userElement.innerHTML = "";
+  userDiv.classList.remove("invisible");
+
+  if (hasAccess) {
+    userElement.innerHTML = "";
+    userElement.insertAdjacentHTML(
+      "beforeend",
+      " <thead><td>Nome</td><td>Email</td></thead>"
+    );
+    for (let i = 0; i < data.length; i++) {
+      userElement.insertAdjacentHTML(
+        "beforeend",
+        "<tr>" +
+          "<td>" +
+          data[i].name +
+          "</td>" +
+          "<td>" +
+          data[i].email +
+          "</td>" +
+          "</tr>"
+      );
+    }
+  } else {
+    userElement.classList.add("invisible");
+    userElement.innerHTML = "";
+  }
+}
+
+function updateDog(data, hasAccess) {
+  const dogElement = document.querySelector(".dog");
+  const dogH6 = document.querySelector(".dog h6");
+  const dogDiv = document.querySelector(".dog div");
+  const dogBtn = document.querySelector(".dog button");
+
+  dogElement.classList.remove("invisible");
+
+  if (hasAccess) {
+    dogH6.innerHTML = "O melhor amigo do homem.";
+    const dogElement = document.querySelector(".dogImage");
+    let dogUrl = data.message;
+    dogElement.insertAdjacentHTML(
+      "beforeend",
+      '<img src="' + dogUrl + '" alt="" >'
+    );
+  } else {
+    dogBtn.classList.add("invisible");
+    dogElement.classList.add("sem-acesso");
+    dogH6.innerHTML = "Sem Acesso";
+    dogDiv.innerHTML = "";
   }
 }
